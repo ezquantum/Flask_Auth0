7c7d174f3067249fa7f69a7de4669cfcd4b43779
Bob Kwon <Amajimoda@bob-2.local>
2020-10-23 00:03:33 -0500

    os.getenv added

--- flaskblogg/auth/auth.py
+++ flaskblogg/auth/auth.py
@@ -26,7 +26,7 @@ oauth = OAuth(app)
 auth0 = oauth.register(
     'auth0',
     client_id=os.environ["AUTH0_DOMAIN"],
-    client_secret=CLIENT_SECRET,
+    client_secret=os.environ["CLIENT_SECRET"],
     api_base_url=API_BASE_URL,
     access_token_url=API_BASE_URL+'/oauth/token',
     authorize_url=API_BASE_URL+'/authorize',
