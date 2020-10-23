1b18eca376ccd84b5173f68b07ff1da3cfd7f987
Bob Kwon <Amajimoda@bob-2.local>
2020-10-22 23:24:36 -0500

    s3 bucket

--- flaskblogg/__init__.py
+++ flaskblogg/__init__.py
@@ -2,15 +2,13 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-from boto.s3.connection import S3Connection
 # from flask_login import LoginManager
 
-s3 = S3Connection(os.environ['SECRET_KEY'], os.environ['DATABASE_URL'])
 # this app is imported
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') #yes it's a dummy site... it's fine
 database_name = "blogatog"
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://epixojdhlwjsir:99617ba473d3f6609a9c93439e87bb31fb1ac9fa6d5d167e66e2e29d703261f0@ec2-52-71-153-228.compute-1.amazonaws.com:5432/d93kgv3fnkj0fg'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 # db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 # login_manager=LoginManager(app)
